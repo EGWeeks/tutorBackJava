ff59150e93a2f3a287134a685554776c80209a40
Ethan Weeks <ethanweeks@ymail.com>
2016-05-25 15:01:31 -0600

    updated Users with basic get and post route examples

--- src/main/java/model/Users.java
+++ src/main/java/model/Users.java
@@ -1,8 +1,22 @@
 package model;
 
+import static spark.Spark.get;
+import static spark.Spark.post;
+
 /**
  * Created by Weeks on 5/25/16.
  */
+
 public class Users {
+    public static void main(String[] args) {
+        get("/", (req, res) -> "This GETS all users.");
+
+        post("/new", (req, res) -> "This is a POST for creating a new user.");
+
+        get("/:id", (req, res) -> "This GETS a users by ID.");
+
+        post("/signin", (req, res) -> "This is a POST for signing a user in.");
+    }
+
     
 }
