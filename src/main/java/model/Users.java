29aaecc4517e6655480504b3e51b8b885df94d91
Ethan Weeks <ethanweeks@ymail.com>
2016-05-27 16:45:12 -0600

    updated Users model with construct and get/setters

--- src/main/java/model/Users.java
+++ src/main/java/model/Users.java
@@ -8,18 +8,109 @@ import static spark.Spark.post;
  */
 
 public class Users {
-    public static void main(String[] args) {
-        get("/", (req, res) -> "This GETS all users.");
 
-        post("/new", (req, res) -> {
-            // req.body() return info as query string
-            return "This is a POST for creating a new user." + req.body();
-        });
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String bio;
+    private String location;
+    private String zip;
+    private String lat;
+    private String lng;
+    private String img;
 
-        get("/:id", (req, res) -> "This GETS a users by ID.");
-
-        post("/signin", (req, res) -> "This is a POST for signing a user in.");
+    public Users(String firstName, String lastName, String email, String password, String bio, String location, String zip, String lat, String lng, String img) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.bio = bio;
+        this.location = location;
+        this.zip = zip;
+        this.lat = lat;
+        this.lng = lng;
+        this.img = img;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
 
 }
